51ac672d3f3856df1d075d0854e6921402da26fb
Martin Solorio <msolorio@berkeley.edu>
2024-03-19 14:07:42 -0700

    Updated README file

--- javascript/constants.js
+++ javascript/constants.js
@@ -3,7 +3,7 @@ export const API_MOVIE_ID_URL = 'https://moviesdatabase.p.rapidapi.com/titles/{i
 export const OPTIONS = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '<your api key>',
+		'X-RapidAPI-Key': '104a5b2fe7mshb1ba7670a3e18bcp1a3cc0jsn662b9239815f',
 		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
 	}
 };
